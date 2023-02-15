5ce51a27085733d686890d736ab9fcb0af5200f0
Alejandro Palmar <thepalmars1one@gmail.com>
2023-02-15 18:47:30 -0400

    responsive first slider and home

--- projects/static/js/scroll.js
+++ projects/static/js/scroll.js
@@ -1,8 +1,11 @@
 const links = document.querySelectorAll('a[href^="#"]');
 links.forEach(link => {
   link.addEventListener('click', e => {
+    console.log("FUNCIONA!");
     e.preventDefault();
     const target = document.querySelector(link.getAttribute('href'));
     target.scrollIntoView({ behavior: 'smooth' });
   });
 });
+
+
